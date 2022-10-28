5f123914e2a098c3a7118abc728de2ecedaf3375
bjornarsomme <bosomme@uia.no>
2022-10-28 19:25:31 +0200

    Refactoring and clean-up

--- Project_AsTree/Project_AsTree.ino
+++ Project_AsTree/Project_AsTree.ino
@@ -2,95 +2,111 @@
 #include "DHT.h"
 #define DHTPIN A0
 #define DHTTYPE DHT11
-#define testPin A5
-#define testPin2 A4
-
-float testValue = 0;
+#define moist1_sen1 A5
+#define moist1_sen2 A4
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int sensorPin = A1;
-int sensorValue = 0;
+const uint8_t PUMP_OFF = HIGH;
+const uint8_t PUMP_ON = LOW;
 
-int motorPin1 = 9;
-int motorPin2 = 6;
+float humidity = 0;
+float airTemp = 0;
 
-char Incoming_value = 0;
-
+float moistVal1 = 0;
+int moist2_sen1 = A1;
+int moistVal2 = 0;
+int pump1 = 9;
+int pump2 = 6;
+char incoming_value = 0;
 bool Watering1 = false;
 bool Watering2 = false;
 
 void setup() {
-  
   Serial.begin(9600);
   dht.begin();
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  digitalWrite(motorPin2, HIGH);
-  digitalWrite(motorPin1, HIGH);
+  pinMode(pump1, OUTPUT);
+  pinMode(pump2, OUTPUT);
+  digitalWrite(pump2, PUMP_OFF);
+  digitalWrite(pump1, PUMP_OFF);
+}
+
+// Getting the moisture value for seconde station
+float getMoisture1() {
+  float val = 0;
+  for (int i = 0; i <= 100; i++) {
+    val = val + analogRead(moist1_sen1) + analogRead(moist1_sen2);
+    delay(1);
+  }
+  val = val / 200;
+  return val;
+}
+
+// Controlling pumps from serial input
+void controlPumps(char input) {
+  if (input == '0') {
+    Watering1 = false;
+    digitalWrite(pump1, PUMP_OFF);
+  } else if (input == '1') {
+    Watering1 = true;
+  } else if (input == '2') {
+    digitalWrite(pump1, PUMP_ON);
+    delay(2000);
+    if (!Watering1) {
+      digitalWrite(pump1, PUMP_OFF);
+    }
+  } else if (input == '3') {
+    Watering2 = false;
+    digitalWrite(pump2, PUMP_OFF);
+  } else if (input == '4') {
+    Watering2 = true;
+  } else if (input == '5') {
+    digitalWrite(pump2, PUMP_ON);
+    delay(2000);
+    if (!Watering2) {
+      digitalWrite(pump2, PUMP_OFF);
+    }
+  }
+}
+
+// Auto watering based on system status and monitored conditions
+void autoWater() {
+  if (Watering1) {
+    if (moistVal2 > 200) {
+      digitalWrite(pump1, PUMP_OFF);
+    } else {
+      digitalWrite(pump1, PUMP_ON);
+    }
+  }
+
+  if (Watering2) {
+    if (moistVal1 < 700) {
+      digitalWrite(pump2, PUMP_OFF);
+    } else {
+      digitalWrite(pump2, PUMP_ON);
+    }
+  }
+}
+
+// Printing to client
+void printString(){
+  Serial.println(String(moistVal2) + "," + humidity + "%," + airTemp + "°C," + moistVal1 + "|");
 }
 
 void loop() {
- 
-  for(int i = 0; i <= 100; i++){
-    testValue = testValue + analogRead(testPin) + analogRead(testPin2);
-    delay(1);
-    
-  }
-  testValue = testValue/200;
+  moistVal1 = getMoisture1();
+  delay(100);
+  moistVal2 = analogRead(moist2_sen1);
+  humidity = dht.readHumidity();
+  airTemp = dht.readTemperature();
 
-  sensorValue = analogRead(sensorPin);
   if (Serial.available() > 0) {
-    Incoming_value = Serial.read();
-     // Serial.print(Incoming_value); ---Fjernet denne!
-     // Serial.print("\n"); ---Fjernet denne!
-    if (Incoming_value == '0') {
-      Watering1 = false;
-      digitalWrite(motorPin1, HIGH);
-    } else if (Incoming_value == '1') {
-      //digitalWrite(motorPin1, LOW);
-      Watering1 = true;
-    } else if (Incoming_value == '2') {
-      digitalWrite(motorPin1, LOW);
-      delay(2000);
-      if(!Watering1){
-        digitalWrite(motorPin1, HIGH);
-      }
+    incoming_value = Serial.read();
+    controlPumps(incoming_value);
+  }
+  if (Watering1 || Watering2) {
+    autoWater();
+  }
 
-      
-    } else if (Incoming_value == '3') {
-      Watering2 = false;
-      digitalWrite(motorPin2, HIGH);
-    } else if (Incoming_value == '4') {
-      Watering2 = true;
-    } else if (Incoming_value == '5') {
-      digitalWrite(motorPin2, LOW);
-      delay(2000);
-      if(!Watering2){
-        digitalWrite(motorPin2, HIGH);
-        }
-      }
-    }
-
-   if(Watering1){
-    if (sensorValue > 200) {
-      digitalWrite(motorPin1, HIGH);
-    } else {
-      digitalWrite(motorPin1, LOW);
-    }
-   }
-
-   if(Watering2){
-    if (testValue < 700){
-      digitalWrite(motorPin2, HIGH);
-    } else {
-      digitalWrite(motorPin2, LOW);
-    }
-   }
-  
-   float humidity = dht.readHumidity();
-   float airTemp = dht.readTemperature();
-   
-   Serial.println(String(sensorValue) + "," + humidity + "%," + airTemp + "°C" + "|");
-
+  printString();
 }
